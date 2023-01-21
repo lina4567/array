c7fea767128c477f8a006aa4bcb77e33f8e90f71
nina <nlina7731@gmail.com>
2023-01-21 12:32:08 -0800

    array example

--- src/main.java
+++ src/main.java
@@ -1,5 +1,22 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+                char consonants[] = new char[5];
+                // b,c,d,f,g;
+                consonants[0] = 'b';
+                consonants[1] = 'c';
+                consonants[2] = 'd';
+                consonants[3] = 'f';
+                consonants[4] = 'g';
+                 System.out.println(consonants[0]);
+        System.out.println(consonants[1]);
+        System.out.println(consonants[2]);
+        System.out.println(consonants[3]);
+        System.out.println(consonants[4]);
+            }
+
+
+
+
+        }
+
+
